a6f942a242cbfe0a37d77319fa0fb4266a187cec
kevin murphy <kevinnealmurphy@gmail.com>
2020-12-11 04:01:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(robot => {
+  const isDecepticon = knownDecepticons.includes(robot.name);
+  return Object.assign({}, robot, {
+    alliance: isDecepticon ? 'decepticon' : 'autobot'
+  });
+});
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +28,5 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map()
